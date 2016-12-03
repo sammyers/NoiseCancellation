911ea9f220cb52a1170e3553e6a4131ead138aa6
Sam Myers <sam@students.olin.edu>
2016-12-03 13:13:39 -0500

    Noise cancelling script now produces a file. Cleaned up some unnecessary files.

--- audio.py
+++ audio.py
@@ -4,57 +4,54 @@ from scipy.io import wavfile
 
 class NoiseCanceller(object):
 
-    def __init__(self, step_size=0.003, num_samples=100):
+    def __init__(self, step_size=0.0003, num_samples=100):
         self.step_size = step_size
         self.num_samples = num_samples
 
-    def load_audio(filepath: str) -> (int, np.array):
+    def load_audio(self, filepath: str) -> (int, np.array):
         """
         Return a numpy array for a given audio file with its sample rate.
         """
-        with wave.open(filepath) as audio_file:
-            sample_rate, audio_array = wavfile.read(audio_file)
+        sample_rate, audio_array = wavfile.read(filepath)
         return sample_rate, audio_array
 
-    def subtract_signal(signal1, signal2):
+    def subtract_signal(self, input_signal, original_signal):
         """
         Return the difference of two audio signals.
         """
-        if signal1.size > signal2.size:
-            signal2.resize(signal1.size)
-        elif signal1.size < signal2.size:
-            signal1.resize(signal2.size)
+        original_signal = np.resize(original_signal, input_signal.shape)
 
-        return signal1 - signal2
+        return input_signal - original_signal
 
-    def compute_filter(prev_value: float, error_value: float, noise_signal: np.array) -> float:
-        """
-        Calculate the next value of the filter signal.
-        """
-        return prev_value + (self.step_size * error_value * noise_signal)
-
-    def filter_signal(error_signal, noise_signal) -> np.array:
+    def filter_signal(self, error_signal, noise_signal) -> np.array:
         """
         Process the error signal based on an adaptive filter.
         """
         filter_output = np.empty_like(error_signal)
         coeffs = np.zeros(self.num_samples)
-
-        for n in range(error_signal.size):
-            moving_signal = noise_signal[n:n-self.num_samples+1:-1]
-            coeffs = coeffs + self.step_size * error_signal[n] * moving_signal
+        for n in range(self.num_samples, error_signal.size):
+            moving_signal = noise_signal[n:n-self.num_samples:-1]
+            coeffs = coeffs - self.step_size * error_signal[n] * moving_signal
             filter_output[n] = np.dot(coeffs, moving_signal)
 
         return filter_output
-        
-    def cancel_noise(input_signal, original_signal, noise_signal):
+
+    def cancel_noise(self, input_signal, original_signal, noise_signal):
         """
         Hopefully cancel some noise.
         """
-        error_signal = subtract_signal(input_signal, original_signal)
-        filtered_signal = filter_signal(error_signal, noise_signal)
+        error_signal = self.subtract_signal(input_signal, original_signal)
+        filtered_signal = self.filter_signal(error_signal, noise_signal)
         new_signal = input_signal - filtered_signal
-        return new_signal
+        return error_signal
 
 if __name__ == '__main__':
-    pass
+    nc = NoiseCanceller()
+    sr, haddaway = nc.load_audio('/Users/sam/Desktop/haddaway.wav')
+    sr2, headphone = nc.load_audio('/Users/sam/git/NoiseCancellation/new_left2.wav')
+    sr3, noise = nc.load_audio('/Users/sam/git/NoiseCancellation/new_right2.wav')
+    haddaway = haddaway[:, 0].flatten()
+    headphone = headphone[:, 0].flatten()
+    noise = noise[:, 1].flatten()
+    cancelled = nc.cancel_noise(headphone, haddaway, noise)
+    wavfile.write('/Users/sam/git/NoiseCancellation/cancelled.wav', sr3, cancelled)
